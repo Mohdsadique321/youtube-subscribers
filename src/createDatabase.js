87faf8d1f26377785cd65f0fe07a0b50d948c5a8
Saqib <mohdsadique312@gmail.com>
2024-04-26 16:22:28 +0530

    add

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,18 +1,28 @@
-const mongoose = require('mongoose')
-const subscriberModel = require('./models/subscribers')
-const data = require('./data')
+/**
+ * @fileOverview Script to refresh subscribers in the database.
+ * @module refreshSubscribers
+ */
+
+const mongoose = require('mongoose');
+const subscriberModel = require('./models/subscribers');
+const data = require('./data');
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb+srv://testforcode:4O1FvTMDv6xbc1DU@cluster0.uxepwdo.mongodb.net/youtube?retryWrites=true&w=majority";
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection
-db.on('error', (err) => console.log(err))
-db.once('open', () => console.log('Database created...'))
+const DATABASE_URL = "mongodb+srv://mohdsadique312:sadique@cluster0.kmaggxd.mongodb.net/";
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err));
+db.once('open', () => console.log('Database connected...'));
 
+/**
+ * Refreshes all subscribers in the database.
+ * @async
+ * @function refreshAll
+ */
 const refreshAll = async () => {
-    await subscriberModel.deleteMany({})
-    // console.log(connection)
-    await subscriberModel.insertMany(data)
+    await subscriberModel.deleteMany({});
+    await subscriberModel.insertMany(data);
     await mongoose.disconnect();
-}
-refreshAll()
+};
+
+refreshAll();
